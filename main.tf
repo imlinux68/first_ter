11eeceac6517fcda760f149365f6a147bf2dbd01
im linux <imlinux68@gmail.com>
2023-05-09 17:50:39 +0300

    updateted subnet block

--- main.tf
+++ main.tf
@@ -12,22 +12,15 @@ provider "aws" {
 }
 
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
   tags = {
     "Name" = "MyVPC"
   }
 }
 
-resource "aws_vpc" "mainvpc" {
-  cidr_block = "192.168.0.0/16"
-  tags = {
-    "Name" = "Main VPC"
-  }
-}
-
 resource "aws_subnet" "sn" {
-    vpc_id = aws_vpc.mainvpc.id
-    cidr_block = "192.168.1.0/24"
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.web_sn
     availability_zone = "us-west-2a"
     tags = {
       "Name" = "Web Subnet"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+  description = "Cidr block for first VPC"
+  type = string
+}
+
+variable "web_sn" {
+  default = "10.0.1.0/24"
+  description = "Cidr block for subnet"
+  type = string
+}
